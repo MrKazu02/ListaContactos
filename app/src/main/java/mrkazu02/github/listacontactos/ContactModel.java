e1b41acb90e42b41750e0adab717ba46288fd49d
Sergi García <kazutoxyt@gmail.com>
2022-04-11 13:54:35 +0200

    commit
    -

---
+++ app/src/main/java/mrkazu02/github/listacontactos/ContactModel.java
@@ -0,0 +1,22 @@
+package mrkazu02.github.listacontactos;
+
+public class ContactModel {
+
+    String name, number;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
